2249b613a0cc216218c17e7f263a52015dd01ee0
JOHNVITORRR <johnvitorgomeslopes.vgl@gmail.com>
2024-12-21 17:37:56 +0000

    ok

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def primeira_pagina():
-    return render_template('index.html')
+def pagina_inicial():
+    return render_template("index.html")
+
+@app.route('/produto')
+def segunda_pagina():
+    return render_template("produto.html")
+
+@app.route('//carinho')
+def terceira_pagina():
+    return render_template("carrinho.html")
+
 
 
